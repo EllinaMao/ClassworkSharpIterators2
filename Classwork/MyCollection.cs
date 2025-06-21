ca81efd43d216096a5b0e646e4d5890175661379
Olga <xotaoshi@gmail.com>
2025-06-21 20:26:11 +0300

    added my class

---
+++ Classwork/MyCollection.cs
@@ -0,0 +1,88 @@
+﻿using Classwork;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+//1. Створіть клас MyClass, в якому створіть автовластивість типу int. Створіть клас MyCollection<T>, який реалізує основний функціонал колекції елементів.При створенні MyCollection <T> використовуйте успадкування та реалізацію IEnumerable<T>, IEnumerator<T>.У методі Main наповніть екземпляр колекції елементами MyClass, виконувати двічі обхід цієї колекції з виведенням на екран значень автовластивості елементів колекції.
+
+
+internal class MyCollection<T> : IEnumerable<T>, IEnumerator<T>
+    {
+
+    public T[] Items;
+    int position = -1;
+    public MyCollection(T[] items)
+    {
+        Items = new T[items.Length];
+        for (int i = 0; i < items.Length; i++)
+        {
+            Items[i] = items[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        return this;
+    }
+
+    bool IEnumerator.MoveNext()
+    {
+        if (position < Items.Length - 1)
+        {
+            position++;
+            return true;
+        }
+        return false;
+    }
+
+    void IEnumerator.Reset()
+    {
+        position = -1;
+    }
+
+    object IEnumerator.Current
+    {
+        get { return Items[position]; }
+    }
+
+    public T Current
+    {
+        get { return Items[position]; }
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+            ((IEnumerator)this).Reset();
+
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+
+    public T this[int index]
+    {
+        get
+        {
+            return Items[index];
+        }
+        set
+        {
+            Items[index] = value;
+        }
+    }
+
+
+    }
+}
